75a97a990443d631eb873393d71216c012ba638b
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-12 22:30:36 +0200

    bootstrap and rootpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   get 'about', to:'about#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root to:'main#index'
 end
