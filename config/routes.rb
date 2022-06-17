58247db96b0324cb7fdd1ea4585ff9b0b08d6caa
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-17 20:20:28 +0200

    signup and fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   get 'about', to:'about#index'
+  get 'signup', to:'registration#new'
+  post 'signup', to:'registration#create'
   
   root to:'main#index'
 end
