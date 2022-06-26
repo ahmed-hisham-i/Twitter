5a0c1513dc56842a429fa1aac8066751d2bf10c3
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-27 00:15:00 +0200

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash[:alert] = "Invalid email or password"
+            redirect_to sign_in_path
+        end
+    end
+
     def destroy
         session[:user_id] = nil
-        redirect_to root_path
+        redirect_to sign_in_path, notice: "Logged out"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@
 
 Rails.application.routes.draw do
   get 'about', to:'about#index'
+
   get 'sign_up', to:'registration#new'
   post 'sign_up', to:'registration#create'
-  delete 'logout', to:'sessions#destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
   
+  delete 'logout', to:'sessions#destroy'
+
   root to:'main#index'
 end
