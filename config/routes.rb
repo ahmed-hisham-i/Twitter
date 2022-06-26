e0fe9038e695b04f4ae2e922b75b49c7f427a4bc
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-26 22:14:11 +0200

    logout button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to:'about#index'
   get 'sign_up', to:'registration#new'
   post 'sign_up', to:'registration#create'
+  delete 'logout', to:'sessions#destroy'
   
   root to:'main#index'
 end
